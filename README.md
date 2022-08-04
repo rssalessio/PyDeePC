363251dbb977cfdab2e7c6d8c7c854b00b317fbe
Alessio Russo <alessior@kth.se>
2022-08-04 11:32:38 +0200

    add files

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # PyDeePC
+
 Python library that implements DeePC: Data-Enabled Predictive Control
+
+_Author_: Alessio Russo (PhD Student at KTH - alessior@kth.se)
+
+## License
+
+Our code is released under the MIT license
+
+## Requirements
+
+- Python 3.7
+## Installation
+
+Use the `setup.py` file to install the library (execute the command `pip install.`)
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+from os import path
+
+
+setup(name = 'PyDeePC',
+    packages=find_packages(),
+    version = '0.0.1',
+    description = 'Python library for Data-Enabled Predictive Control',
+    url = 'https://github.com/rssalessio/PyDeePC',
+    author = 'Alessio Russo',
+    author_email = 'alessior@kth.se',
+    install_requires=['numpy', 'scipy'],
+    license='MIT',
+    zip_safe=False,
+    python_requires='>=3.7',
+)
