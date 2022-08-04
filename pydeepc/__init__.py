daca89b5dbeb5f212a8145888b3d006d369a6799
Alessio Russo <alessior@kth.se>
2022-08-04 17:29:24 +0200

    add info to init file

--- pydeepc/__init__.py
+++ pydeepc/__init__.py
@@ -1,2 +1,9 @@
 from .deepc import *
 from .utils import *
+
+__author__ = 'Alessio Russo - alessior@kth.se'
+__version__ = '0.0.1'
+__info__ = {
+    'version': __version__,
+    'author': __author__
+}
